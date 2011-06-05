21de054489ddc3f7f4f6a42df24e31e485b66cf6
Ignacio Thayer <ignacio@cookie.(none)>
2011-06-05 14:13:26 -0700

    Updates

--- project.clj
+++ project.clj
@@ -8,6 +8,8 @@
                                               com.sun.jmx/jmxri]]
                  [clj-time "0.3.0"]
                  [ring "0.3.8"]
-		 [compojure "0.6.2"]]
+		 [compojure "0.6.2"]
+		 [aleph "0.2.0-alpha1"]
+		 [lamina "0.4.0-alpha2-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :main clomeque.core)
