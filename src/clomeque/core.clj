42510747d1319508a897a8ed083e3431130ba0a2
Prashanth <djunforgetable@gmail.com>
2011-06-05 20:30:56 -0400

    Support multiple queues in the system

--- src/clomeque/core.clj
+++ src/clomeque/core.clj
@@ -16,35 +16,77 @@
   (:use compojure.core)
   (:gen-class))
 
-(def broadcast-channel (channel 1))
+;; a Queue actually consists of a lamina channel and out delay buffer
+(defrecord Queue [channel delay-buffer])
 
-(defn success-response []
+;; a message, probably needs to become a protocol to support tostring
+(defrecord Message [timestamp message])
+
+;; queue constructor, simple
+(defn create-queue []
+  (Queue. (channel) []))
+
+;; a ref to the queues in the system (do we need a ref?)
+(def queues (ref {}))
+
+;; TODO: content-types ... should be configurable?
+(defn success-response [msg]
   {:status 200
    :headers {"content-type" "text/plain"}
-   :body "success"})
+   :body msg})
 
-(defn read-channel [res-channel req]
+;; TODO: error responses aren't always 404s !
+(defn error-response [msg]
+  {:status 404
+   :headers {"content-type" "text/plain"}
+   :body msg})
+
+;; read a message from a queue (block if not there)
+(defn read-queue [dest-queue res-channel req]
   (lg/info (str "Got read channel request" req))
-  (receive broadcast-channel 
+  (receive (:channel dest-queue) 
 	   #(enqueue res-channel {:status 200
 				  :headers {"content-type" "text/plain"}
 				  :body (str %)})))
 
-(defn write-to-channel [res-channel req]
+;; write a message to queue
+(defn write-queue [dest-queue res-channel req]
   "Will write a message (obtained from the body of the request)
    to the single broadcast channel"
+  ;; TODO: need to submit a message, not just the body
   (let [body (channel-buffer->string (input-stream->channel-buffer (:body req)))]
     (println "Got body" body)
     ; enqueue a message into the broad-cast channel
-    (enqueue broadcast-channel body)
+    (enqueue (:channel dest-queue) body)
+    ;; TODO: need to write to delay buffer as well!
+    ;(conj (:delay-buffer) body)
     ; send back a success response
-    (enqueue res-channel (success-response))))
+    (enqueue res-channel (success-response "Message submitted!"))))
 
 (defroutes web-routes
   (GET "/channels/:channel-name" [channel-name]
-       (wrap-aleph-handler read-channel))
+       (let [q (get @queues channel-name)]
+	 (if q
+	   (wrap-aleph-handler (partial read-queue q))
+	   (error-response (str "Channel" channel-name "does not exist")))))
+  (PUT "/channels/:channel-name" [channel-name]
+       (dosync
+	(if (@queues channel-name)
+	  (error-response "Queue already exists")
+	  (let [queues (alter queues assoc channel-name (create-queue))]
+	    (success-response (str "Queue" channel-name "created!"))))))
   (POST "/channels/:channel-name" [channel-name]
-	(wrap-aleph-handler write-to-channel))
+	(let [q (get @queues channel-name)]
+	  (if q
+	    (wrap-aleph-handler (partial write-queue q))
+	    (error-response (str "Channel" channel-name "does not exist")))))
+  (DELETE "/channels/:channel-name" [channel-name]
+	  (dosync
+	   (if (@queues channel-name)
+	     (do
+	       (alter queues dissoc channel-name)
+	       (success-response (str "Queue" channel-name "was deleted!")))
+	     (error-response (str "Queue" channel-name "does not exist!")))))
   (route/not-found "Page not found"))
 
 (defn -main [& args]
